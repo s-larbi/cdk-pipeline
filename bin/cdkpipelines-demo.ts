5fafe650abffbb092be3d7f4148c2808321013ec
s-larbi <s.larbi@zeroandone.me>
2021-02-13 23:38:55 +0400

    first commit

---
+++ bin/cdkpipelines-demo.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+
+const app = new App();
+
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '194865251619', region: 'eu-west-1' },
+});
+
+app.synth();
